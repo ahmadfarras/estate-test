8f447d69d79e30911af24decc963b3171cf12a99
Farras <ahmadfarrassyafrin@gmail.com>
2025-02-05 11:08:13 +0700

    Fixing error unit test

--- handler/endpoints.go
+++ handler/endpoints.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/SawitProRecruitment/UserService/generated"
@@ -11,6 +12,14 @@ import (
 
 	apiError "github.com/SawitProRecruitment/UserService/error"
 )
+
+// This is just a test endpoint to get you started. Please delete this endpoint.
+// (GET /hello)
+func (s *Server) GetHello(ctx echo.Context, params generated.GetHelloParams) error {
+	var resp generated.HelloResponse
+	resp.Message = fmt.Sprintf("Hello User %d", params.Id)
+	return ctx.JSON(http.StatusOK, resp)
+}
 
 // GetEstateIdDronePlan implements generated.ServerInterface.
 func (s *Server) GetEstateIdDronePlan(ctx echo.Context, id uuid.UUID) error {
